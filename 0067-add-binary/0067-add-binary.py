217b9c9626e87eb8eb8a63b8a0ec65b241a238da
Vivek617 <vivekreddykeesara@gmail.com>
2026-09-25 11:23:24 +0530

    Time: 1 ms (66.45%), Space: 12.4 MB (23.67%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+
+            if i >= 0:
+                total += ord(a[i]) - 48
+                i -= 1
+
+            if j >= 0:
+                total += ord(b[j]) - 48
+                j -= 1
+
+            result.append(str(total % 2))
+            carry = total // 2
+
+        return ''.join(reversed(result))
